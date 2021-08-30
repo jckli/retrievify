8e81e8b73e43784322ebeedc4c1ffa9471137370
Jack Pee <mcinvasion2016@gmail.com>
2021-08-29 21:08:24 -0700

    Fixed Indentation Problems and little bugs

--- analysis.py
+++ analysis.py
@@ -7,6 +7,11 @@ class spotifyInfo:
     milsec = 0
     endTime = 0
 
+# Converts milsecond time to mins
+def convertMillitoMin(millis):
+    minutes=(millis/(1000*60))
+    return minutes
+
 # Gets all the files and puts all the songs into classes instances 
 # and into lists that contain total time played separated by years
 def get_info(dirName):
@@ -40,7 +45,7 @@ def get_list(streamingHistory, dirname):
 
                 # Checks for Duplicates and adds time
                 for existing in listA:
-                    if(item["trackName"] == existing.name and item["artistName"] == existing.artist and item["endTime"][0:4] == existing.endTime[0:4]):
+                    if(item["trackName"] == existing.song and item["artistName"] == existing.artist and item["endTime"][0:4] == existing.endTime[0:4]):
                         existing.milsec += item["msPlayed"]
                         dupIndicate = True
                 
@@ -57,63 +62,59 @@ def get_list(streamingHistory, dirname):
                 dupIndicate = False
     return listA
 
-    # Returns a list that contains all the songs in a given year
-    def yearSort(list, year):
-        a = []
-        for i in list:
-            if(year == i.endTime[0:4]):
-                a.append(i)
-        return a
+# Returns a list that contains all the songs in a given year
+def yearSort(list, year):
+    a = []
+    for i in list:
+        if(year == i.endTime[0:4]):
+            a.append(i)
+    return a
     
-    # Returns Top number of Songs based on time from list
-    def topList(list, int):
-        listA = []
-        for i in list:
-            for top in listA:
-                if( i.milsec > top.milsec and len(listA) >= int):
-                    listA.append(i)
-                    sorted(listA)
-                    listA.pop()
-                elif(len(listA) < int):
-                    listA.append(i)
-        return listA
+# Returns Top number of Songs based on time from list
+def topList(list, int):
+    listA = []
+    for i in list:
+        for top in listA:
+            if( i.milsec > top.milsec and len(listA) >= int):
+                listA.append(i)
+                sorted(listA)
+                listA.pop()
+            elif(len(listA) < int):
+                listA.append(i)
+    return listA
 
-    # Returns list of Top Int Song Names
-    def topIntSongNames(list,int):
-        listA = topList(list, int)
-        listB = []
-        for i in listA:
-            listB.append(i.song)
-        return listB
+# Returns list of Top Int Song Names
+def topIntSongNames(list,int):
+    listA = topList(list, int)
+    listB = []
+    for i in listA:
+        listB.append(i.song)
+    return listB
 
-    # Defaults to top 10 Song Names
-    def topSongNames(list):
-        return topIntSongNames(list, 10)
+# Defaults to top 10 Song Names
+def topSongNames(list):
+    return topIntSongNames(list, 10)
     
-    # Returns the name of the top 10 artist names
-    def topIntArtistNames(list, int):
-        listA = topList(list, int)
-        listB = []
-        for i in listA:
-            listB.append(i.artist)
-        return listB
+# Returns the name of the top 10 artist names
+def topIntArtistNames(list, int):
+    listA = topList(list, int)
+    listB = []
+    for i in listA:
+        listB.append(i.artist)
+    return listB
 
-    # Defaults to top 10 Artist Names
-    def topArtistNames(list):
-        return topIntArtistNames(list, 10)
+# Defaults to top 10 Artist Names
+def topArtistNames(list):
+    return topIntArtistNames(list, 10)
 
-    # Returns Total milseconds listen on spotify
-    def totalTimeListened(list):
-        a = 0
-        for i in list:
-            a += i.milsec
-        return a
+# Returns Total milseconds listen on spotify
+def totalTimeListened(list):
+    a = 0
+    for i in list:
+        a += i.milsec
+    return a
     
-    # Returns Total milseconds listen on spotify for a given year
-    def totalTimeListenedInYear(list, year):
-        return totalTimeListened.yearSort(list, year)
+# Returns Total milseconds listen on spotify for a given year
+def totalTimeListenedInYear(list, year):
+    return totalTimeListened.yearSort(list, year)
 
-    # Converts milsecond time to mins
-    def convertMillitoMin(millis):
-        minutes=(millis/(1000*60))
-        return minutes
