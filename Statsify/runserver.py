8a857f8edbd47cf592ef1de5189c49c2afc24780
Jack Pee <mcinvasion2016@gmail.com>
2021-12-13 23:58:57 -0700

    Added Oauth2 Authentication

--- Statsify/runserver.py
+++ Statsify/runserver.py
@@ -4,8 +4,11 @@ This script runs the Statsify application using a development server.
 
 from os import environ
 from Statsify import app
+from dotenv import load_dotenv
 
 if __name__ == '__main__':
+    app.config.from_pyfile('settings.py')
+    app.secret_key = "6upNRfAhgf5sL1SW7KOIGDngu4cJ7gas"
     HOST = environ.get('SERVER_HOST', 'localhost')
     DEBUG=True
     try:
