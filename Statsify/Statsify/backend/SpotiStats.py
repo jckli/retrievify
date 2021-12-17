dd9fc99099fed4bb9b1ade859970ce5922e6b07e
Jack Pee <mcinvasion2016@gmail.com>
2021-12-17 04:29:17 -0700

    Updated terminal app interface

--- Statsify/Statsify/backend/SpotiStats.py
+++ Statsify/Statsify/backend/SpotiStats.py
@@ -10,7 +10,7 @@ from os import system, name
 import json
 
 # Start Up Menu
-print("SpotiStats v1")
+print("SpotiStats v1.1")
 print("Starting up.")
 sleep(0.05)
 
@@ -35,22 +35,10 @@ def clear():
 
 list = get_info(dirname)
 sleep(1)
-#clear()
+clear()
 
 #print("First Time instance: " + firstTime)
 #print("Last Time instance: " + currentYear)
 
 #print(f"Your minutes listened in 2021 is {timeListened2021:,.2f} minutes. That's a lot OwO")
-print("~ What would you like to see?")
-print("1. Top Songs")
-print("2. Top Artists")
-print("3. Top Genre (Not working - prolly gonna use api)")
-print("4. Total Time Listened on Spotify")
-print("5. Total Time Listened in a Year")
-print("6. Total Time Listened to Song")
-print("7. Total Time Listened to Song in a Year")
-print("8. Total Time Listened to Artist")
-print("9. Total Time Listened to Artist in a Year")
-
-userInput = input("Enter a Number: ")
-choices(list, userInput)
+choices(list)
