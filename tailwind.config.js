a816b8af626b6755fef91ccb99a4cda04accb302
lina <jack@jackli.dev>
2022-11-17 11:33:28 -0700

    chore: add fonts+logo

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@ module.exports = {
     content: ["./src/**/*.{js,ts,jsx,tsx}"],
     darkMode: true, // or 'media' or 'class'
     theme: {
-        extend: {},
+        extend: {
+            colors: {
+                primary: "#4ad3ff",
+            },
+            fontFamily: {
+                proximaNova: ["Proxima Nova"],
+                gotham: ["Gotham"],
+            },
+        },
     },
     variants: {
         extend: {},
