a3527ad0219cb0e9becfedb1cac9cad358459964
lina <jack@jackli.dev>
2022-11-18 22:06:31 -0700

    fix: api index page

--- api/index.py
+++ api/index.py
@@ -6,8 +6,9 @@ import urllib
 app = Sanic("Statsify")
 
 
-@app.route("/")
+@app.route("/api")
+@app.route("/api/index")
 async def index(request):
     return response.json(
-        {"Hey!": "This is the Statsify API! Please go back to the main website."}
+        {"Hey!": "This is the Statsify API. Please go back to the main website."}
     )
