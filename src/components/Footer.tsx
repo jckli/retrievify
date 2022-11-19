d3d88a21701fa87ea0167785a0c9d1449ff65f8b
lina <jack@jackli.dev>
2022-11-19 13:31:47 -0700

    refactor: move footer into component

---
+++ src/components/Footer.tsx
@@ -0,0 +1,32 @@
+import Link from "next/link";
+
+export const Footer = () => {
+    const currentYear = new Date().getFullYear();
+    return (
+        <>
+            <div className="h-[10vh] flex justify-center items-center font-gotham font-normal">
+                <div className="text-white flex flex-col md:flex-row justify-between items-center xl:w-[1140px] lg:w-[930px] md:w-[720px] sm:w-[530px]">
+                    <div className="inline-block">
+                        Made with ❤️ by{" "}
+                        <a className="inline-block" href="https://github.com/jckli">
+                            jckli
+                        </a>{" "}
+                        &{" "}
+                        <a className="inline-block" href="https://github.com/The-Brandon-Tran">
+                            BTrident
+                        </a>
+                        .
+                    </div>
+                    <div>
+                        <Link href="/privacy">
+                            <a className="text-[#808080] mr-2 underline underline-offset-2 decoration-2 decoration-[#808080] inline-block">
+                                Privacy Policy
+                            </a>
+                        </Link>
+                        © 2021 - {currentYear} Statsify
+                    </div>
+                </div>
+            </div>
+        </>
+    );
+};
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,9 @@
 import type { NextPage } from "next";
 import Image from "next/image";
 import { MainButton } from "../components/MainButton";
-import Link from "next/link";
+import { Footer } from "../components/Footer";
 
 const Home: NextPage = () => {
-    const currentYear = new Date().getFullYear();
     return (
         <>
             <div className="w-full h-[90vh] flex flex-col items-center justify-center text-white text-3xl font-semibold text-center">
@@ -22,29 +21,7 @@ const Home: NextPage = () => {
                     <MainButton text="Login" href="/api/login" />
                 </div>
             </div>
-            <div className="h-[10vh] flex justify-center items-center font-gotham font-normal">
-                <div className="text-white flex flex-col md:flex-row justify-between items-center xl:w-[1140px] lg:w-[930px] md:w-[720px] sm:w-[530px]">
-                    <div className="inline-block">
-                        Made with ❤️ by{" "}
-                        <a className="inline-block" href="https://github.com/jckli">
-                            jckli
-                        </a>{" "}
-                        &{" "}
-                        <a className="inline-block" href="https://github.com/The-Brandon-Tran">
-                            BTrident
-                        </a>
-                        .
-                    </div>
-                    <div>
-                        <Link href="/privacy">
-                            <a className="text-[#808080] mr-2 underline underline-offset-2 decoration-2 decoration-[#808080] inline-block">
-                                Privacy Policy
-                            </a>
-                        </Link>
-                        © 2021 - {currentYear} Statsify
-                    </div>
-                </div>
-            </div>
+            <Footer />
         </>
     );
 };
