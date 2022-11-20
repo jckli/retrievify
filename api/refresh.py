714eb0c210fb47e9437c973cf05a0826a6297ea4
lina <jack@jackli.dev>
2022-11-19 21:05:33 -0700

    feat: refresh + getuser

---
+++ api/refresh.py
@@ -0,0 +1,30 @@
+from .index import app
+from .utils._spotify import Spotify
+from sanic import response
+import os
+import secrets
+import urllib
+
+
+@app.route("/api/refresh")
+async def refresh_token(request):
+    no_access = response.json({"error": {"status": 401, "message": "No access"}})
+    if not request.cookies.get("acct"):
+        return no_access
+    if not request.cookies.get("reft"):
+        return no_access
+    if not request.cookies.get("exp"):
+        return no_access
+    refresh_token = request.cookies.get("reft")
+    expires_in = int(request.cookies.get("exp"))
+    if expires_in > 0:
+        return response.json({"error": {"status": 401, "message": "Not expired"}})
+    spotify = Spotify()
+    token_resp = await spotify.refresh_token(refresh_token)
+    if token_resp is None:
+        return response.redirect("/api/login")
+    resp = response.json({"data": {"status": 200, "message": "Refreshed"}})
+    resp.cookies["acct"] = token_resp["access_token"]
+    resp.cookies["reft"] = token_resp["refresh_token"]
+    resp.cookies["exp"] = str(token_resp["expires_in"])
+    return resp
--- api/utils/_spotify.py
+++ api/utils/_spotify.py
@@ -13,6 +13,9 @@ class Spotify:
         self.base64_message = base64_bytes.decode("ascii")
         self.session = aiohttp.ClientSession()
 
+    async def close(self):
+        await self.session.close()
+
     async def get_token(self, code):
         url = "https://accounts.spotify.com/api/token"
         auth = f"Basic {self.base64_message}"
@@ -33,5 +36,31 @@ class Spotify:
             else:
                 return None
 
-    async def close(self):
-        await self.session.close()
+    async def refresh_token(self, refresh_token):
+        url = "https://accounts.spotify.com/api/token"
+        auth = f"Basic {self.base64_message}"
+        headers = {
+            "Authorization": auth,
+            "Content-Type": "application/x-www-form-urlencoded",
+            "Accept": "application/json",
+        }
+        data = {
+            "refresh_token": refresh_token,
+            "grant_type": "refresh_token",
+        }
+        async with self.session.post(url, headers=headers, data=data) as resp:
+            if resp.status == 200:
+                return await resp.json()
+            else:
+                return None
+
+    async def get_user(self, access_token):
+        url = "https://api.spotify.com/v1/me"
+        headers = {"Authorization": f"Bearer {access_token}"}
+        async with self.session.get(url, headers=headers) as resp:
+            if resp.status == 200:
+                return await resp.json()
+            elif resp.status == 401:
+                return 401
+            else:
+                return None
