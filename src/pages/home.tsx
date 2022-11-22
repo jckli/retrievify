aa8ab44868b9eaee14140397973e624807b6ce38
lina <jack@jackli.dev>
2022-11-21 21:11:04 -0700

    feat: top artists

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -8,7 +8,8 @@ import { faSpotify } from "@fortawesome/free-brands-svg-icons";
 import { PeriodDropdown } from "../components/PeriodDropdown";
 
 const Home: NextPage = () => {
-    const [period, setPeriod] = useState("short_term");
+    const [periodGenre, setPeriodGenre] = useState("short_term");
+    const [periodArtist, setPeriodArtist] = useState("short_term");
     const fetcher = (url: any) => fetch(url).then(r => r.json());
 
     // Fetch currently playing data
@@ -69,62 +70,102 @@ const Home: NextPage = () => {
             <Sidebar active={1} />
             <div className="navbar:ml-[280px] flex font-metropolis text-white">
                 <div className="m-8 flex flex-col 1.5xl:flex-row">
-                    <div id="now-playing" className="bg-mgray rounded-md 1.5xl:min-w-[50%] h-fit">
-                        <div className="p-5">
-                            <h1 className="font-proximaNova text-3xl">Now Playing</h1>
-                            <div className="mt-4">
-                                {currently_playing ? (
-                                    <div className="flex flex-col text-center xsm:text-left xsm:flex-row">
-                                        <div className="m-auto xsm:mx-0">
-                                            <div className="relative h-[128px] w-[128px]">
-                                                <Image
-                                                    alt="albumArt"
-                                                    draggable={false}
-                                                    src={currently_playing?.item.album.images[0].url}
-                                                    layout="fill"
-                                                />
+                    <div className="1.5xl:w-[50%] flex flex-col">
+                        <div id="now-playing" className="bg-mgray rounded-md 1.5xl:min-w-[50%] h-fit">
+                            <div className="p-5">
+                                <h1 className="font-proximaNova text-3xl">Now Playing</h1>
+                                <div className="mt-4">
+                                    {currently_playing ? (
+                                        <div className="flex flex-col text-center xsm:text-left xsm:flex-row">
+                                            <div className="m-auto xsm:mx-0">
+                                                <div className="relative h-[128px] w-[128px]">
+                                                    <Image
+                                                        alt="albumArt"
+                                                        draggable={false}
+                                                        src={currently_playing?.item.album.images[0].url}
+                                                        layout="fill"
+                                                    />
+                                                </div>
+                                            </div>
+                                            <div className="mt-4 xsm:mt-0 xsm:ml-4">
+                                                <h1 className="text-2xl">{currently_playing?.item.name}</h1>
+                                                <h2>
+                                                    {currently_playing?.item.artists
+                                                        .map((artist: any) => artist.name)
+                                                        .join(", ")}
+                                                </h2>
+                                                <a
+                                                    href={currently_playing?.item.external_urls.spotify}
+                                                    className="block mt-1"
+                                                >
+                                                    <FontAwesomeIcon icon={faSpotify} size="lg" />
+                                                </a>
                                             </div>
                                         </div>
-                                        <div className="mt-4 xsm:mt-0 xsm:ml-4">
-                                            <h1 className="text-2xl">{currently_playing?.item.name}</h1>
-                                            <h2>
-                                                {currently_playing?.item.artists
-                                                    .map((artist: any) => artist.name)
-                                                    .join(", ")}
-                                            </h2>
-                                            <a
-                                                href={currently_playing?.item.external_urls.spotify}
-                                                className="block mt-1"
-                                            >
-                                                <FontAwesomeIcon icon={faSpotify} size="lg" />
-                                            </a>
+                                    ) : (
+                                        <div className="flex flex-col">
+                                            <h1 className="text-2xl">Nothing Playing</h1>
                                         </div>
+                                    )}
+                                </div>
+                            </div>
+                        </div>
+                        <div id="top-artists" className="bg-mgray rounded-md mt-8">
+                            <div className="p-5">
+                                <div className="flex items-center justify-between sm:justify-start">
+                                    <h1 className="font-proximaNova text-3xl">Top Artists</h1>
+                                    <div className="ml-4">
+                                        <PeriodDropdown setPeriod={setPeriodArtist} />
                                     </div>
-                                ) : (
+                                </div>
+                                <div className="mt-4">
                                     <div className="flex flex-col">
-                                        <h1 className="text-2xl">Nothing Playing</h1>
+                                        {topArtists[periodArtist].items
+                                            .slice(0, 10)
+                                            .map((artist: any, index: number) => (
+                                                <div key={index} className="flex items-center mt-4">
+                                                    <div>
+                                                        <div className="relative h-[64px] w-[64px]">
+                                                            <Image
+                                                                alt="albumArt"
+                                                                draggable={false}
+                                                                src={artist.images[0].url}
+                                                                layout="fill"
+                                                            />
+                                                        </div>
+                                                    </div>
+                                                    <div className="ml-4">
+                                                        <h1 className="text-2xl">{artist.name}</h1>
+                                                        <h2>
+                                                            {artist.genres.length > 0 ? artist.genres[0] : "No Genre"}
+                                                        </h2>
+                                                    </div>
+                                                </div>
+                                            ))}
                                     </div>
-                                )}
+                                </div>
                             </div>
                         </div>
                     </div>
-                    <div id="top-genres" className="bg-mgray rounded-md mt-8 1.5xl:ml-8 1.5xl:mt-0">
-                        <div className="p-5">
-                            <div className="flex items-center justify-between sm:justify-start">
-                                <h1 className="font-proximaNova text-3xl">Top Genres</h1>
-                                <div className="ml-4">
-                                    <PeriodDropdown setPeriod={setPeriod} />
+                    <div className="1.5xl:w-[50%] flex flex-col">
+                        <div id="top-genres" className="bg-mgray rounded-md mt-8 1.5xl:ml-8 1.5xl:mt-0">
+                            <div className="p-5">
+                                <div className="flex items-center justify-between sm:justify-start">
+                                    <h1 className="font-proximaNova text-3xl">Top Genres</h1>
+                                    <div className="ml-4">
+                                        <PeriodDropdown setPeriod={setPeriodGenre} />
+                                    </div>
                                 </div>
-                            </div>
-                            <div className="mt-4">
-                                <div className="flex flex-wrap gap-4">
-                                    {topGenres[period].slice(0, 10).map((genre: string, index: number) => (
-                                        <div key={index + 1} className="bg-[#404040] rounded-lg">
-                                            <h1 className="text-2xl m-2 mx-3">
-                                                {index + 1}. {genre}
-                                            </h1>
-                                        </div>
-                                    ))}
+                                <div className="mt-4">
+                                    <div className="flex flex-wrap gap-4">
+                                        {topGenres[periodGenre].slice(0, 10).map((genre: string, index: number) => (
+                                            <div key={index + 1} className="bg-[#404040] rounded-lg">
+                                                <h1 className="text-2xl m-2 mx-3">
+                                                    {index + 1}. {genre}
+                                                </h1>
+                                            </div>
+                                        ))}
+                                    </div>
                                 </div>
                             </div>
                         </div>
@@ -138,6 +179,7 @@ const Home: NextPage = () => {
 export default Home;
 
 interface TopItems {
+    [key: string]: any;
     short_term: any;
     medium_term: any;
     long_term: any;
