e3e3bb95c0a79ba9779c605d00829f951c64f27e
lina <me@jackli.dev>
2023-01-13 23:32:07 -0700

    refactor: error handling swr

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -61,7 +61,7 @@ const Home: NextPage = () => {
     };
     const topGenres = get_top_genres(topArtists);
 
-    if (error2 || error3 || error4 || error5 || error6 || error7) {
+    if (error1 || error2 || error3 || error4 || error5 || error6 || error7) {
         return (
             <div className="flex w-[100vw] h-[100vh] items-center justify-center text-white font-proximaNova">
                 Loading...
