9d028102618aea77207d5af319ef8172bb6a45c7
lina <me@jackli.dev>
2023-02-05 22:47:19 -0700

    refactor: only post requests

--- api/src/spotify/routes.py
+++ api/src/spotify/routes.py
@@ -7,34 +7,34 @@ bp = Blueprint("Spotify", url_prefix="/spotify")
 async def spotify_index(request):
     return response.json({"status": 200, "message": "Statsify API v1 - Spotify Module", "link": "https://statsify.hayasaka.moe"})
 
-@bp.route("/currentlyplaying")
+@bp.route("/currentlyplaying", methods=["POST"])
 async def currently_playing(request):
     return await currentlyplaying.currently_playing(request)
 
-@bp.route("/getuser")
+@bp.route("/getuser", methods=["POST"])
 async def get_user(request):
     return await getuser.get_user(request)
 
-@bp.route("/topitems/<type>")
+@bp.route("/topitems/<type>", methods=["POST"])
 async def top_items(request, type: str):
     return await topitems.top_items(request, type)
 
-@bp.route("/tracks/<id>")
+@bp.route("/tracks/<id>", methods=["POST"])
 async def get_track(request, id: str):
     return await tracks.get_track(request, id)
 
-@bp.route("/tracks/<id>/audiofeatures")
+@bp.route("/tracks/<id>/audiofeatures", methods=["POST"])
 async def get_track_audio_features(request, id: str):
     return await tracks.get_track_audio_features(request, id)
 
-@bp.route("/artists/<id>")
+@bp.route("/artists/<id>", methods=["POST"])
 async def get_artist(request, id: str):
     return await artists.get_artist(request, id)
 
-@bp.route("/artists/<id>/toptracks")
+@bp.route("/artists/<id>/toptracks", methods=["POST"])
 async def get_artist_top_tracks(request, id: str):
     return await artists.get_artist_top_tracks(request, id)
 
-@bp.route("/artists/<id>/relatedartists")
+@bp.route("/artists/<id>/relatedartists", methods=["POST"])
 async def get_related_artists(request, id: str):
     return await artists.get_artist_related_artists(request, id)
