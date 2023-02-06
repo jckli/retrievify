5a2788d43b1848d4da1bb7e19de133de9a9c01c1
lina <me@jackli.dev>
2023-02-06 10:18:20 -0700

    fix: new refresh auth methods

--- api/src/spotify/artists/toptracks.py
+++ api/src/spotify/artists/toptracks.py
@@ -7,29 +7,42 @@ async def get_artist_top_tracks(request, id):
     body = request.json
     if body is None:
         return no_access
-    if (body.get("access_token") is None) or (body.get("refresh_token") is None):
+    if body.get("refresh_token") is None:
         return no_access
     access_token = body.get("access_token")
     spotify = Spotify()
+    if access_token is None:
+        return await get_artist_top_tracks_refresh(
+            spotify, body.get("refresh_token"), id
+        )
     user_resp = await spotify.get_artist_top_tracks(access_token, id, "US")
     resp = response.json({"status": 200, "data": user_resp})
     if user_resp is None:
         await spotify.close()
         return response.json({"status": 404, "message": "No data"})
     elif user_resp == 401:
-        ref = await spotify.refresh_token(body.get("refresh_token"))
-        if "error" in ref:
-            await spotify.close()
-            return no_access
-        access_token = ref.get("access_token")
-        user_resp = await spotify.get_artist_top_tracks(access_token, id, "US")
-        resp = response.json(
-            {
-                "status": 201,
-                "data": user_resp,
-                "access_token": access_token,
-                "expires_in": ref["expires_in"],
-            }
+        return await get_artist_top_tracks_refresh(
+            spotify, body.get("refresh_token"), id
         )
+    await spotify.close()
+    return resp
+
+
+async def get_artist_top_tracks_refresh(spotify, refresh_token, id):
+    no_access = response.json({"status": 401, "message": "No access"})
+    ref = await spotify.refresh_token(refresh_token)
+    if ref == 401 or ref == None:
+        await spotify.close()
+        return no_access
+    access_token = ref.get("access_token")
+    user_resp = await spotify.get_artist_top_tracks(access_token, id, "US")
+    resp = response.json(
+        {
+            "status": 201,
+            "data": user_resp,
+            "access_token": access_token,
+            "expires_in": ref.get("expires_in"),
+        }
+    )
     await spotify.close()
     return resp
