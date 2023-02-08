aa02e707b17d5c47f693f86ca5af80fef3658fed
lina <me@jackli.dev>
2023-02-07 22:48:21 -0700

    fix: aight i gotta be trolling

--- api/src/spotify/tracks/multiaudiofeatures.py
+++ api/src/spotify/tracks/multiaudiofeatures.py
@@ -1,10 +1,11 @@
 from ...utils._spotify import Spotify
 from sanic import response
+import urllib.parse
 
 
 async def get_tracks_audio_features(request):
     no_access = response.json({"status": 401, "message": "No access"})
-    ids = request.args.get("ids")
+    ids = urllib.parse.quote(request.args.get("ids"))
     body = request.json
     if body is None:
         return no_access
@@ -16,7 +17,7 @@ async def get_tracks_audio_features(request):
         return await get_tracks_audio_features_refresh(
             spotify, body.get("refresh_token"), ids
         )
-    user_resp = await spotify.get_track_audio_features(access_token, ids)
+    user_resp = await spotify.get_tracks_audio_features(access_token, ids)
     resp = response.json({"status": 200, "data": user_resp})
     if user_resp is None:
         await spotify.close()
@@ -36,7 +37,7 @@ async def get_tracks_audio_features_refresh(spotify, refresh_token, ids):
         await spotify.close()
         return no_access
     access_token = ref.get("access_token")
-    user_resp = await spotify.get_track_audio_features(access_token, ids)
+    user_resp = await spotify.get_tracks_audio_features(access_token, ids)
     resp = response.json(
         {
             "status": 201,
