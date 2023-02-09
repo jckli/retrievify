33117355c4e6702f65d4733bbf3f2d76577453eb
lina <me@jackli.dev>
2023-02-09 07:29:30 -0700

    chore: add hayasaka.moe domain

--- api/main.py
+++ api/main.py
@@ -3,7 +3,12 @@ from sanic_ext import Extend
 from src.routes import add_routes
 
 app = Sanic("Statsify")
-app.config.CORS_ORIGINS = ["http://localhost:3000", "https://statsify.hayasaka.moe", "https://statsify-beta.vercel.app"]
+app.config.CORS_ORIGINS = [
+    "http://localhost:3000",
+    "https://statsify.hayasaka.moe",
+    "https://sbeta.hayasaka.moe",
+    "https://statsify-beta.vercel.app",
+]
 add_routes(app)
 
 if __name__ == "__main__":
